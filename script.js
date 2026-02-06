b05a2b922d74acd0f6402eb7817c5cd8b29a4f3a
tiagomelchert <tiagomelchert@gmail.com>
2026-02-05 21:54:56 -0300

    atualização menu

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+  const toggle = document.querySelector('.menu-toggle');
+  const menu = document.querySelector('.menu');
+  toggle.addEventListener('click', () => {
+    menu.classList.toggle('show');
+  });
+  
 // Inicializar Ícones Lucide
 lucide.createIcons();
 
